a2186448922e0981f7bc3d2fac3ea9134e1304f8
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-02-05 10:25:44 +0100

    #10 Made skeleton of DisplayPage

---
+++ src/components/DisplayPage/DisplayPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DisplayPage = () => {
+    return (
+        <>
+            <h1>Title</h1>
+            <p>Intro</p>
+            {/* Need to get markdown parser */}
+        </>
+    )
+}
+
+export default DisplayPage;
