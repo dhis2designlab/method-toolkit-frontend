40020b03b6a5a639309a54b911ef4611af44dd09
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-02-06 20:34:31 +0100

    #10 fetched from API and displayed info

--- src/components/DisplayPage/DisplayPage.tsx
+++ src/components/DisplayPage/DisplayPage.tsx
@@ -1,13 +1,39 @@
-import React from "react";
-
-const DisplayPage = () => {
-    return (
-        <>
-            <h1>Title</h1>
-            <p>Intro</p>
-            {/* Need to get markdown parser */}
-        </>
-    )
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import { fetchTechnique } from "../../api/TechniquesAPI";
+interface displayPage {
+  id: string;
 }
+
+const DisplayPage = ({ id }: displayPage) => {
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchTechnique(id);
+      setResult(await data?.json());
+    };
+
+    fetchData();
+  }, [id]);
+
+  return (
+    <>
+      {result.length !== 0 ? (
+        result.map((item: any) => {
+          return (
+            <div key={item.id}>
+              <h1>{item.title}</h1>
+              <p>{item.intro}</p>
+              <ReactMarkdown children={item.content} />
+            </div>
+          );
+        })
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
+};
 
 export default DisplayPage;
