0836fb21bab62a04cd4a1ac48ee6f86ed957b51b
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-02-10 11:30:18 +0100

    #11 Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import LandingPage from "./components/LandingPage/LandingPage";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <LandingPage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./Footer.module.css";
+
+const Footer = (): JSX.Element => {
+    return (
+      <footer className={styles.footer}>
+        <p className={styles.text}>DHIS2 Design Lab | University of Oslo</p>
+      </footer>
+    );
+}
+
+export default Footer;
