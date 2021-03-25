a8a0105aaf6e766e27049a1c8762ec0231d402b3
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-03-25 16:44:01 +0100

    #41 Added filtering for techniques and activities

---
+++ src/data/enums.ts
@@ -0,0 +1,11 @@
+export enum resourceTypes {
+    ACTIVITIES = "Activities",
+    TECHNIQUES = "Techniques",
+    ALL = "All"
+};
+
+export enum techniqueDifficulties {
+    EASY = "Easy",
+    MODERATE = "Moderate",
+    HARD = "Hard"
+};
