3062a6e7987e12644bf4badcadef7814855b2d48
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-10-11 12:39:12 +0200

    Removed uneccesary div

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -15,13 +15,11 @@ export const ErrorMessage = ({
     "Could not fetch the content for the page. Please try again later."
 
   return (
-    <div className={styles.errorMessageContainer}>
-      <div className={styles.errorMessages}>
-        <Alert severity="error">
-          <AlertTitle>{title ?? genericTitle}</AlertTitle>
-          {description ?? genericDescription}
-        </Alert>
-      </div>
+    <div className={styles.errorMessages}>
+      <Alert severity="error">
+        <AlertTitle>{title ?? genericTitle}</AlertTitle>
+        {description ?? genericDescription}
+      </Alert>
     </div>
   )
 }
